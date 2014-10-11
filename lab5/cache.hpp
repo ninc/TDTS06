23c1b7e9821011e9f63127970324a46f1f0dad44
Ninc <jacsu246@student.liu.se>
2014-10-11 13:47:23 +0000

    Skipping comments and whitespace in input file now

--- lab5/cache.hpp
+++ lab5/cache.hpp
@@ -1,6 +1,8 @@
 #ifndef CACHE_HPP
 #define CACHE_HPP
 
+enum cache_type {LRU, TTL};
+
 using namespace std;
 class cache{
 public:
--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,5 +1,7 @@
 #include "cache.hpp"
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -7,18 +9,23 @@ int main( int argc, const char* argv[] )
 {
   int k = argc;
 
-  cache c = cache();
-  
-
   cout << k << endl;
   
   for(int i = 0; i<k; i++)
     {
+      //LOOK FOR LRU or TTL scheme
       cout << argv[i] << " ";
     }
 
   cout << endl;
 
+
+  //Create new cache object with LRU or TTL scheme
+  cache c = cache();
+
+  string line;
+  size_t comment;
+  stringstream ss;
   int time_stamp;
   string client_id;
   int video_duration;
@@ -28,11 +35,33 @@ int main( int argc, const char* argv[] )
   int priority;
 
 
-  while(cin >> time_stamp >> client_id >> video_duration >> server_name >> file_name >> file_size >> priority)
+  while(getline(cin, line))
     {
-      cout << time_stamp << " " << client_id << " ";
-      cout << video_duration << " " << server_name << " ";
-      cout << file_name << " " << file_size << " " << priority << endl;
+
+      comment = line.find("#");   
+      //If comment or empty line, skip line
+      if(comment != string::npos || line.length() == 0)
+	{
+	  //cout << line << endl;
+	  continue;
+	}
+      //Handle input
+      else
+	{
+	  //Read input
+	  ss << line;
+	  ss >> time_stamp >> client_id >> video_duration >> server_name >> file_name >> file_size >> priority;
+  
+	  //Cache input
+	  //cache.add(time_stamp, client_id, video_duration, server_name, file_name, file_size, priority);
+
+	  //Output
+	  cout << time_stamp << " " << client_id << " ";
+	  cout << video_duration << " " << server_name << " ";
+	  cout << file_name << " " << file_size << " " << priority << endl;
+	  
+	    
+	}
     }
 
 
